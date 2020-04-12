cc361b112c36ec312b22abde479bb166ceb613b7
marsggbo <1435679023@qq.com>
2020-04-12 15:26:29 +0800

    format all files

--- visualize.py
+++ visualize.py
@@ -1,7 +1,7 @@
-import numpy as np 
-import torch 
-from genotypes import * 
-from visualize_ftn import * 
+import numpy as np
+import torch
+from genotypes import *
+from visualize_ftn import *
 from option.default_option import TrainOptions
 opt = TrainOptions()
 
@@ -13,12 +13,12 @@ alpha_reduce = np.load('alpha_npy/alpha_reduce_' + str(epoch) + '.npy')
 
 
 m_normal = torch.distributions.relaxed_categorical.RelaxedOneHotCategorical(
-    torch.tensor([temperature]), torch.tensor(alpha_normal)) 
+    torch.tensor([temperature]), torch.tensor(alpha_normal))
 m_reduce = torch.distributions.relaxed_categorical.RelaxedOneHotCategorical(
-    torch.tensor([temperature]) , torch.tensor(alpha_reduce)) 
+    torch.tensor([temperature]), torch.tensor(alpha_reduce))
 
 alpha_normal = m_normal.sample().cpu().numpy()
 alpha_reduce = m_reduce.sample().cpu().numpy()
-ex = genotype(alpha_normal,alpha_reduce)
-plot(ex.normal,'normal.pdf')
-plot(ex.reduce,'reduce.pdf')
+ex = genotype(alpha_normal, alpha_reduce)
+plot(ex.normal, 'normal.pdf')
+plot(ex.reduce, 'reduce.pdf')
